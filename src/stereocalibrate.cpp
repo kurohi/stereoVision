adf2eb4b125c71928e73d7b12fb87abcfaaf3921
Igor Coelho <kagken.kurohi@gmail.com>
2016-07-04 23:22:26 +0900

    Finish updating the stereocalibrate to be useful with files as well. although the files saved are resulting into a wrong calibration

--- src/stereocalibrate.cpp
+++ src/stereocalibrate.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
+#include <string>
+#include <iomanip>
 #include <TwinCamera.hpp>
 
 using namespace cv;
@@ -30,52 +32,59 @@ int main(int argc, char* argv[])
     cv::Mat left_img, right_img, left_gray, right_gray;
     std::string folder_root;
     TwinCamera *twin;
-    if((argc >= 6)&&(argv[4] == "-f")){
+    if((argc >= 6)&&(std::string(argv[4]) == "-f")){
+	std::cout<<"Using images for calibration"<<std::endl;
 	folder_root = std::string(argv[5]);
     }else{
         twin = new TwinCamera(0,1);
     }
 
-    int success = 0, k = 0 img_index =0;
+    int success = 0, k = 0, img_index =0;
     bool found1 = false, found2 = false;
 
     while (success < numBoards)
     {
-    	if((argc >= 6)&&(argv[4] == "-f")){
-	    std::ostringstream left_adds, right_addrs;
-	    left_addrs << folder_root << "left_" << std::setw(3) << std::setfill('0') << img_index;
+    	if((argc >= 6)&&(std::string(argv[4]) == "-f")){
+	    std::ostringstream left_addrs, right_addrs;
+	    left_addrs << folder_root << "left_" << std::setw(2) << std::setfill('0') << img_index << ".jpg";
+	    std::cout<<left_addrs.str()<<std::endl;
             left_img = cv::imread(left_addrs.str());
-	    right_addrs << folder_root << "right_" << std::setw(3) << std::setfill('0') << img_index;
-	    right_img = cv::imread(right_addrs);
-
+	    right_addrs << folder_root << "right_" << std::setw(2) << std::setfill('0') << img_index << ".jpg";
+	    std::cout<<right_addrs.str()<<std::endl;
+	    right_img = cv::imread(right_addrs.str());
+	    if((right_img.empty())||(left_img.empty())){
+                break;
+	    }
 	}else{
             twin->getDoubleImages(left_img,right_img);
 	}
         cv::cvtColor(left_img, left_gray, CV_BGR2GRAY);
         cv::cvtColor(right_img, right_gray, CV_BGR2GRAY);
 
-        found1 = cv::findChessboardCorners(left_img, board_sz, corners1, cv::CV_CALIB_CB_ADAPTIVE_THRESH | cv::CV_CALIB_CB_FILTER_QUADS);
-        found2 = cv::findChessboardCorners(right_img, board_sz, corners2, cv::CV_CALIB_CB_ADAPTIVE_THRESH | cv::CV_CALIB_CB_FILTER_QUADS);
+        found1 = cv::findChessboardCorners(left_img, board_sz, corners1, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+        found2 = cv::findChessboardCorners(right_img, board_sz, corners2, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
 
         if (found1)
         {
-            cv::cornerSubPix(left_gray, corners1, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(cv::CV_TERMCRIT_EPS | cv::CV_TERMCRIT_ITER, 30, 0.1));
-            cv::drawChessboardCorners(left_gray, board_sz, corners1, found1);
+            cv::cornerSubPix(left_gray, corners1, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+            cv::drawChessboardCorners(left_img, board_sz, corners1, found1);
         }
 
         if (found2)
         {
-            cv::cornerSubPix(right_gray, corners2, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(cv::CV_TERMCRIT_EPS | cv::CV_TERMCRIT_ITER, 30, 0.1));
-            cv::drawChessboardCorners(right_gray, board_sz, corners2, found2);
+            cv::cornerSubPix(right_gray, corners2, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+            cv::drawChessboardCorners(right_img, board_sz, corners2, found2);
         }
         
-        cv::imshow("image1", left_gray);
-        cv::imshow("image2", right_gray);
+        cv::imshow("left", left_img);
+        cv::imshow("right", right_img);
 
         k = cv::waitKey(10);
-        if (found1 && found2)
+        if ((argc >= 6)&&(std::string(argv[4]) == "-f"))
         {
-            k = cv::waitKey(0);
+            //k = cv::waitKey(0);
+	    k = ' ';
+	    img_index++;
         }
         if (k == 27)
         {
@@ -96,7 +105,15 @@ int main(int argc, char* argv[])
             }
         }
     }
-    delete(twin);
+    if((argc < 6)||(std::string(argv[4]) != "-f")){
+        delete(twin);
+    }else{
+        std::ostringstream left_addrs, right_addrs;
+        left_addrs << folder_root << "left_00.jpg";
+        left_img = cv::imread(left_addrs.str());
+        right_addrs << folder_root << "right_00.jpg";
+        right_img = cv::imread(right_addrs.str());
+    }
 
     cv::destroyAllWindows();
     printf("Starting Calibration\n");
@@ -107,8 +124,8 @@ int main(int argc, char* argv[])
 
     float result = cv::stereoCalibrate(object_points, imagePoints1, imagePoints2, 
                     CM1, D1, CM2, D2, left_img.size(), R, T, E, F, 
-                    cv::CV_CALIB_SAME_FOCAL_LENGTH | cv::CV_CALIB_ZERO_TANGENT_DIST,
-                    cv::TermCriteria(cv::CV_TERMCRIT_ITER+cv::CV_TERMCRIT_EPS, 100, 1e-5));
+                    CV_CALIB_SAME_FOCAL_LENGTH | CV_CALIB_ZERO_TANGENT_DIST,
+                    cv::TermCriteria(CV_TERMCRIT_ITER+CV_TERMCRIT_EPS, 100, 1e-5));
     
     std::cout<<"Result from calibration: "<<result<<std::endl;
 
@@ -141,31 +158,39 @@ int main(int argc, char* argv[])
     cv::Mat map1x, map1y, map2x, map2y;
     cv::Mat imgU1, imgU2;
 
-    cv::initUndistortRectifyMap(CM1, D1, R1, P1, left_img.size(), cv::CV_32FC1, map1x, map1y);
-    cv;:initUndistortRectifyMap(CM2, D2, R2, P2, right_img.size(), cv::CV_32FC1, map2x, map2y);
+    cv::initUndistortRectifyMap(CM1, D1, R1, P1, left_img.size(), CV_32FC1, map1x, map1y);
+    cv::initUndistortRectifyMap(CM2, D2, R2, P2, right_img.size(), CV_32FC1, map2x, map2y);
 
     printf("Undistort complete\n");
 
+    img_index = 0;
     while(1)
     {    
-       	if((argc >= 6)&&(argv[4] == "-f")){
-	    std::ostringstream left_adds, right_addrs;
-	    left_addrs << folder_root << "left_" << std::setw(3) << std::setfill('0') << img_index;
+       	if((argc >= 6)&&(std::string(argv[4]) == "-f")){
+	    std::ostringstream left_addrs, right_addrs;
+	    left_addrs << folder_root << "left_" << std::setw(2) << std::setfill('0') << img_index << ".jpg";
             left_img = cv::imread(left_addrs.str());
-	    right_addrs << folder_root << "right_" << std::setw(3) << std::setfill('0') << img_index;
-	    right_img = cv::imread(right_addrs);
+	    right_addrs << folder_root << "right_" << std::setw(2) << std::setfill('0') << img_index << ".jpg";
+	    right_img = cv::imread(right_addrs.str());
+            if((right_img.empty())||(left_img.empty())){
+	        break;
+	    }
 
 	}else{
             twin->getDoubleImages(left_img,right_img);
 	}
 
-        cv::remap(left_img, imgU1, map1x, map1y, cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar());
-        cv::remap(right_img, imgU2, map2x, map2y, cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar());
+        cv::remap(left_img, imgU1, map1x, map1y, INTER_LINEAR, BORDER_CONSTANT, cv::Scalar());
+        cv::remap(right_img, imgU2, map2x, map2y, INTER_LINEAR, BORDER_CONSTANT, cv::Scalar());
 
-        cv::imshow("image1", imgU1);
-        cv::imshow("image2", imgU2);
+        cv::imshow("left", imgU1);
+        cv::imshow("right", imgU2);
 
         k = cv::waitKey(5);
+	if((argc >= 6)&&(std::string(argv[4]) == "-f")){
+	    k = cv::waitKey(0);
+	    img_index++;
+	}
 
         if(k==27)
         {
