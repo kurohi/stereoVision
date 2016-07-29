9e90023007f61f614dd3c97523eca7ea4cff9748
kurohi <kageken.kurohi@gmail.com>
2016-07-29 13:50:48 +0900

    added a little library to save the pointclouds into meshlab files )

---
+++ include/WriteToMesh.hpp
@@ -0,0 +1,18 @@
+#ifndef WRTIE_TO_MESH_HPP_
+#define WRTIE_TO_MESH_HPP_
+#include <General.hpp>
+#include <fstream>
+#include <opencv2/calib3d/calib3d.hpp>
+
+class WriteToMesh{
+    public:
+        static void writeDisparityToMeshRaw(cv::Mat disparity, cv::Mat Qmatrix, std::string filename);
+        static void writeDisparityToMeshProcessed(cv::Mat point_cloud, cv::Mat disparity, std::string filename);
+        static void writeWithColorToMeshRaw(cv::Mat disparity, cv::Mat rgb_img, cv::Mat Qmatrix, std::string filename);
+        static void writeWithColorToMeshProcessed(cv::Mat point_cloud, cv::Mat disparity, cv::Mat rgb_img, std::string filename);
+    private:
+        WriteToMesh() { }
+};
+
+
+#endif
---
+++ lib/WriteToMesh.cpp
@@ -0,0 +1,88 @@
+#include <WriteToMesh.hpp>
+
+void WriteToMesh::writeDisparityToMeshProcessed(cv::Mat point_cloud, cv::Mat disparity, std::string filename){
+    std::ofstream plyfile(filename.c_str());
+    if(!plyfile.is_open()){
+        throw std::runtime_error("File not found on saveComputedPoints");
+    }
+    int failed_pixel = 0;
+    for(int y=0; y<disparity.rows; y++)
+        for(int x=0; x<disparity.cols; x++){
+            if(disparity.at<uchar>(y,x)==0){
+                failed_pixel++;
+            }
+        }
+    int point_num = (int)point_cloud.total();
+    plyfile << "ply" << std::endl;
+    plyfile << "format ascii 1.0" << std::endl;
+    plyfile << "element vertex " << point_num - failed_pixel << std::endl;
+    plyfile << "property float x" << std::endl;
+    plyfile << "property float y" << std::endl;
+    plyfile << "property float z" << std::endl;
+    plyfile << "end_header" << std::endl;
+
+    cv::Point3f point;
+    for(int y=0; y<point_cloud.rows; y++)
+        for(int x=0; x<point_cloud.cols; x++){
+            if(disparity.at<uchar>(y,x)==0){
+                continue;
+            }
+            point = point_cloud.at<cv::Point3f>(y,x);
+            plyfile << point.x << " " << point.y << " " << point.z << std::endl;
+        }
+    plyfile.close();
+
+}
+
+void WriteToMesh::writeDisparityToMeshRaw(cv::Mat disparity, cv::Mat Qmatrix, std::string filename){
+    cv::Mat point_cloud;
+    cv::reprojectImageTo3D(disparity, point_cloud, Qmatrix);
+    writeDisparityToMeshProcessed(point_cloud, disparity, filename); 
+}
+
+void WriteToMesh::writeWithColorToMeshRaw(cv::Mat disparity, cv::Mat rgb_img, cv::Mat Qmatrix, std::string filename){
+    cv::Mat point_cloud;
+    cv::reprojectImageTo3D(disparity, point_cloud, Qmatrix);
+    WriteToMesh::writeWithColorToMeshProcessed(point_cloud, disparity, rgb_img, filename);
+}
+
+void WriteToMesh::writeWithColorToMeshProcessed(cv::Mat point_cloud, cv::Mat disparity, cv::Mat rgb_img, std::string filename){
+    std::ofstream plyfile(filename.c_str());
+    if(!plyfile.is_open()){
+        throw std::runtime_error("File not found on saveComputedPoints");
+    }
+    int failed_pixel = 0;
+    for(int y=0; y<disparity.rows; y++)
+        for(int x=0; x<disparity.cols; x++){
+            if(disparity.at<uchar>(y,x)==0){
+                failed_pixel++;
+            }
+        }
+    int point_num = (int)point_cloud.total();
+    plyfile << "ply" << std::endl;
+    plyfile << "format ascii 1.0" << std::endl;
+    plyfile << "element vertex " << point_num - failed_pixel << std::endl;
+    plyfile << "property float x" << std::endl;
+    plyfile << "property float y" << std::endl;
+    plyfile << "property float z" << std::endl;
+    plyfile << "property uchar red" << std::endl;
+    plyfile << "property uchar green" << std::endl;
+    plyfile << "property uchar blue" << std::endl;
+    plyfile << "end_header" << std::endl;
+
+    cv::Point3f point;
+    for(int y=0; y<point_cloud.rows; y++)
+        for(int x=0; x<point_cloud.cols; x++){
+            if(disparity.at<uchar>(y,x)==0){
+                continue;
+            }
+            point = point_cloud.at<cv::Point3f>(y,x);
+            plyfile << point.x << " " << point.y << " " << point.z << " ";
+            plyfile << (int)rgb_img.at<cv::Vec3b>(y,x)[2] << " " <<
+            (int) rgb_img.at<cv::Vec3b>(y,x)[1] << " " << (int)rgb_img.at<cv::Vec3b>(y,x)[0] << std::endl;
+
+        }
+    plyfile.close();
+
+
+}
