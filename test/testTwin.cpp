a4536bb4e93ea97b4647b6d7e22bbe28b23daa42
kurohi <kageken.kurohi@gmail.com>
2016-09-09 13:55:58 +0900

    commented out the line that asked for the calibration file so that the twin camera test behave as just a camera capture program

--- test/testTwin.cpp
+++ test/testTwin.cpp
@@ -2,11 +2,11 @@
 
 int main(int argc, char **argv){
 	TwinCamera twin(0,1);
-	Mat img1, img2;
+    cv::Mat img1, img2;
 	twin.getDoubleImages( img1, img2);
-	twin.loadCameraParameters(argv[1], img1, img2);
+	//twin.loadCameraParameters(argv[1], img1, img2);
 	twin.rectifyForStereo(img1,img2);
-	imwrite("image1.jpg", img1);
-	imwrite("image2.jpg", img2);
+    cv::imwrite("image1.jpg", img1);
+    cv::imwrite("image2.jpg", img2);
 	return 0;
 }
